e34c4d679bb198177595b6ad6bab5737b7bc4f82
Daniel <danielsamet98@gmail.com>
2021-08-06 16:33:00 +0100

    agents now write their names to file

--- proj/agents/template.py
+++ proj/agents/template.py
@@ -1,20 +1,30 @@
+import os
 from abc import abstractmethod
+
 from ..model.state import GameState, Action
 
+
 class ThudAgentTemplate:
+    agents_filename = "agents.txt"
+
     def __init__(self, name, agentClassName) -> None:
         self.name = name
         self.agentClassName = agentClassName
-    
+
+        with open(ThudAgentTemplate.agents_filename, "r") as file:
+            agents = file.read()
+
+        if self.agentClassName not in agents:
+            with open(ThudAgentTemplate.agents_filename, "a" if os.path.isfile(ThudAgentTemplate.agents_filename) else "w") as file:
+                file.write(self.agentClassName)
+
     def __str__(self) -> str:
         return f'{self.name} ({self.agentClassName})'
-    
+
     def __repr__(self) -> str:
         return str(self)
-    
-    
+
     @abstractmethod
-    def act(self, state: GameState, game_number:int, wins:dict)-> Action:
+    def act(self, state: GameState, game_number: int, wins: dict) -> Action:
         ''' select an action according to the gameState and return it '''
         pass
-
